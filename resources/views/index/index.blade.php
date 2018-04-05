6ec3f16edeb5fbf4a2cdcff94bf4f5436cb4efbb
Kazushi Takamura <sakamatapd5@gmail.com>
2018-04-05 23:57:21 +0900

    Add:  コイン送信済みページデザイン編集

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -12,23 +12,26 @@
         </div>
     </div>
 
-    <div id="qr_res_message"></div>
-
-    <form id="qr_form">
-        <input type=text id="qr_url" size=1 placeholder="Tracking Code" class=qrcode-text>
+    <div class="card mx-auto mt-3 pb-3">
+        <div id="qr_res_message" class="text-center p-3"></div>
+        <h2 class="mt-3">残高 : {{number_format($user->now_point)}}Pt</h2>
+        <form id="qr_form">
+            <input type=text id="qr_url" size=1 placeholder="Tracking Code" class=qrcode-text>
             <label class=qrcode-text-btn mx-auto>
                 <input type=file accept="image/*" capture=environment onclick="return showQRIntro();" onchange="openQRCamera(this);" tabindex=-1>
             </label>
-    </form>
+        </form>
 
-    <div id="send_form" class="mx-auto" style="display:none">
-        <button type="button" class="btn btn-lg btn-warning d-block mx-auto"  onclick=checkQRpath()>あげる</button>
+        <div id="send_form" class="mx-auto mt-3" style="display:none">
+            <button type="button" class="btn btn-lg btn-warning d-block mx-auto"  onclick=checkQRpath()>あげる</button>
+        </div>
+        <div class="d-flex mt-3">
+            <a id="btn_back" style="display:none" role="button" href="/" class="btn btn-lg btn-warning mx-auto">もどる</a>
+        </div>
+        <div class="d-flex mt-3">
+            <a role="button" href="/" class="btn btn-lg btn-warning mx-auto disabled">別の人にあげる</a>
+        </div>
     </div>
-
-    <button type="button" class="btn btn-lg btn-warning d-block mx-auto">別の人にあげる</button>
-
-    <h2 class="mt-3">残高 : {{number_format($user->now_point)}}Pt</h2>
-
 </div>
 
 <script type="text/javascript">
@@ -43,9 +46,10 @@ function openQRCamera(node) {
         node.parentNode.previousElementSibling.value = res;
 
         var message = document.getElementById("qr_res_message");
-        message.innerHTML = "<p>QRコードを読み取りました『あげる』を押してください</p>";
+        message.innerHTML = "<div class='alert alert-warning text-center mt-5 mb-5'>QRコードを読み取りました<br>『あげる』を押してください</div>";
         document.getElementById("qr_form").style.display="none";
         document.getElementById("qr_receve").style.display="none";
+        document.getElementById("btn_back").style.display="inline";
         document.getElementById("send_form").style.display="block";
       }
     };
--- resources/views/index/send.blade.php
+++ resources/views/index/send.blade.php
@@ -15,37 +15,46 @@
         </div>
     </div>
     <div class="card mx-auto mt-3 pb-3">
+        <table class="table table-borderless text-right mb-0 balance_table">
+            <tr>
+                <th>残高 :</th>
+                <td>{{number_format($user->now_point)}}&nbsp;Pt</td>
+            </tr>
+            <tr>
+                <th><span id="full_point"></span>送信額 :</th>
+                <td><mark><span id="sum_point" >0</span><mark>&nbsp;Pt</td>
+            </tr>
+            <tr>
+                <th>送信後残高 :</th>
+                <td><span id="after_point">{{number_format($user->now_point)}}</span>&nbsp;Pt</td>
+            </tr>
+        </table>
         <form action="/post" method="post">
             {{ csrf_field() }}
             <!-- style非表示でフォームを持つ、JSで値を入れ、coinの額を送信する -->
             <input type="number" id="send_point" name="send_point" style="display:none;">
             <input type="hidden" name="receve_user_id" value="{{$send_user->id}}">
-
-            <h2 class="mx-auto mt-3">
-                <span>送信額 : <span id="sum_point">0</span> Pt </span><span id="full_point"></span>
+            <div id="send_form" class="text-center mt-3">
+                <button type="button" class="btn btn-warning" onclick=sendPointSum(10000)>10000</button>
+                <button type="button" class="btn btn-warning" onclick=sendPointSum(1000)>1000</button>
+                <button type="button" class="btn btn-warning" onclick=sendPointSum(100)>100</button>
+                <button type="button" class="btn btn-warning" onclick=sendPointSum(10)>10</button>
+                <button type="button" class="btn btn-warning" onclick=sendPointSum(1)>1</button>
+            </div>
+            <div class="text-center mt-4">
+                <button type="button" onclick=sendPointSum(0) class="btn btn-lg btn-info mr-5">リセット</button>
                 <span id="send_btn" style="display:none;">
-                    <button type="submit" class="btn btn-lg btn-warning mx-auto">あげる！</button>
+                    <button type="submit" class="btn btn-lg btn-warning">&nbsp;あげる&nbsp;</button>
                 </span>
                 <span id="send_btn_disabled">
-                    <button type="submit" class="btn btn-lg btn-warning mx-auto" disabled>あげる</button>
+                    <button type="submit" class="btn btn-lg btn-warning" disabled>&nbsp;あげる&nbsp;</button>
                 </span>
-            </h2>
+            </div>
         </form>
-        <div id="send_form" class="text-center mt-3">
-            <button type="button" class="btn btn-warning" onclick=sendPointSum(10000)>10000</button>
-            <button type="button" class="btn btn-warning" onclick=sendPointSum(1000)>1000</button>
-            <button type="button" class="btn btn-warning" onclick=sendPointSum(100)>100</button>
-            <button type="button" class="btn btn-warning" onclick=sendPointSum(10)>10</button>
-            <button type="button" class="btn btn-warning" onclick=sendPointSum(1)>1</button>
-        </div>
-        <button type="button" onclick=sendPointSum(0) class="mt-3 btn btn-info d-block mx-auto">リセット</button>
     </div>
-    <h2 class="mt-3">残高 : {{number_format($user->now_point)}}Pt</h2>
-    <h2 class="mt-3">送信後残高 : <span id="after_point">{{number_format($user->now_point)}}</span>Pt</h2>
 </div>
 
 <script type="text/javascript">
-
 // クロージャで送信ポイントを貯める、リセットする、表示を切り替える
 var send_point_sum = function() {
     var sum = 0;
@@ -81,6 +90,5 @@ var send_point_sum = function() {
     }
 }
 var sendPointSum = send_point_sum();
-
 </script>
 @endsection
